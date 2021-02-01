d0e3d01a2a2a818b9aa6f6038f87e1f4e48aac1f
JJS <1029650454@qq.com>
2021-02-01 23:02:54 +0800

    用户模板

---
+++ sp-user/sp-user-controller/src/main/java/user/Application.java
@@ -0,0 +1,22 @@
+package user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * order:Junsen
+ * Time:2021-1-31 00:19:32
+ *
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class Application {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class,args);
+    }
+}
+
